b1714b35a7cd38eb3ded4cd0f8ca3b815761aed6
LexMari <legosmurf@outlook.com>
2024-04-23 14:21:36 +0100

    Updated to implement batch normalization, altered training epochs and learning rate changed

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,14 +1,10 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-from sklearn.impute import SimpleImputer
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.pipeline import Pipeline
 from keras import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, BatchNormalization
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 data = pd.read_csv('C:/Users/legos/Downloads/Dataset of Diabetes.csv')
@@ -28,27 +24,34 @@ data['Gender'] = label_encoder.fit_transform(data['Gender'])
 data['CLASS'].unique()
 data['Gender'].unique()
 
-X = data.values
 Y = data['CLASS'].values
-data.to_csv('Diabetes3Pls.csv', index=False)
+X = data.drop(columns=['CLASS']).values
+# data.to_csv('Diabetes4hope.csv', index=False)
 
 X = np.delete(X, 1, axis=1)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
 
-tf.convert_to_tensor(X_train, dtype=tf.float32)
-tf.convert_to_tensor(Y_train, dtype=tf.float32)
+X_train = tf.convert_to_tensor(X_train, dtype=tf.float32)
+X_test = tf.convert_to_tensor(X_test, dtype=tf.float32)
+Y_train = tf.convert_to_tensor(Y_train, dtype=tf.float32)
 
 classifier = Sequential()
-classifier.add(Dense(units=10, activation='relu', input_dim=X.shape[1]))
-classifier.add(Dense(units=10, activation='relu'))
+classifier.add(Dense(units=64, activation='relu', input_dim=X.shape[1]))
+classifier.add(Dense(units=32, activation='relu'))
+classifier.add(BatchNormalization())
 classifier.add(Dense(units=1, activation='sigmoid'))
-classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-classifier.fit(X_train, Y_train, epochs=100, batch_size=10)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+classifier.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+
+classifier.fit(X_train, Y_train, epochs=100, batch_size=32, verbose=1)
 
 Y_pred = classifier.predict(X_test)
 Y_pred_int = (Y_pred > 0.5).astype(int)
+
 cm = confusion_matrix(Y_test, Y_pred_int)
 acc = accuracy_score(Y_test, Y_pred_int)
+
 print("Accuracy:", acc)
 print("Confusion Matrix:\n", cm)
